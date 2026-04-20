bc8645c07865028f8230b8a3d9083e689aa08872
thevibecoder13 <akshitamahapatro13@gmail.com>
2026-04-20 22:42:42 +0530

    Add function to count even and odd numbers
    Implemented a function to count even and odd numbers in a list.

--- q22.py
+++ q22.py
@@ -6,3 +6,15 @@
 •	Return both counts from the function. 
 •	Display the results. 
 '''
+def count(text):
+  even_count = 0
+  odd_count = 0
+  for num in text:
+    if num % 2==0:
+      even_count += 1
+    else:
+      odd_count +=1
+  return even_count, odd_count
+x = list(map(int, input("Enter nos: ").split()))
+output = count(x)
+print(output)
